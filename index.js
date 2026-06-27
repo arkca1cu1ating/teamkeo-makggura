538431f8020f30a8017cf792a42513a22439e41c
arkca1cu1ating <ark.ca1cu1ating@gmail.com>
2026-06-28 03:39:18 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAIl6sNYPBSVnfyug6GXFF9SafgieqdXSE",
-  authDomain: "teamkeo-makggura.firebaseapp.com",
-  databaseURL: "https://teamkeo-makggura-default-rtdb.firebaseio.com/",
-  projectId: "teamkeo-makggura"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
-function createRoom(){
- let code=Math.random().toString(36).substring(2,8);
- db.ref('rooms/'+code).set({ready:{p1:false,p2:false}});
- location.href='room.html?room='+code+'&role=p1';
-}
-
-function joinRoom(){
- let code=document.getElementById('code').value;
- location.href='room.html?room='+code+'&role=p2';
-}
+const firebaseConfig={apiKey:"AIzaSyAIl6sNYPBSVnfyug6GXFF9SafgieqdXSE",authDomain:"teamkeo-makggura.firebaseapp.com",databaseURL:"https://teamkeo-makggura-default-rtdb.firebaseio.com/",projectId:"teamkeo-makggura"};
+firebase.initializeApp(firebaseConfig);const db=firebase.database();
+function createRoom(){let code=Math.random().toString(36).substring(2,8);db.ref('rooms/'+code).set({result:null,ready:{p1:false,p2:false}});location.href='room.html?room='+code+'&role=p1';}
+function joinRoom(){let code=document.getElementById('code').value;location.href='room.html?room='+code+'&role=p2';}
--- room.js
+++ room.js
@@ -1,29 +1,5 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAIl6sNYPBSVnfyug6GXFF9SafgieqdXSE",
-  authDomain: "teamkeo-makggura.firebaseapp.com",
-  databaseURL: "https://teamkeo-makggura-default-rtdb.firebaseio.com/",
-  projectId: "teamkeo-makggura"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
-const params=new URLSearchParams(location.search);
-const room=params.get('room');
-const role=params.get('role');
-
-document.getElementById('room').innerText="Room:"+room;
-
-function select(r){
- db.ref('rooms/'+room+'/result').set({role:role,result:r});
-}
-
-function agree(){
- db.ref('rooms/'+room+'/ready/'+role).set(true);
- db.ref('rooms/'+room+'/ready').once('value').then(s=>{
-  let d=s.val();
-  if(d.p1&&d.p2){
-   location.href='log.html?room='+room;
-  }
- });
-}
+const firebaseConfig={apiKey:"AIzaSyAIl6sNYPBSVnfyug6GXFF9SafgieqdXSE",authDomain:"teamkeo-makggura.firebaseapp.com",databaseURL:"https://teamkeo-makggura-default-rtdb.firebaseio.com/",projectId:"teamkeo-makggura"};
+firebase.initializeApp(firebaseConfig);const db=firebase.database();
+const params=new URLSearchParams(location.search);const room=params.get('room');const role=params.get('role');document.getElementById('room').innerText='Room:'+room;
+function select(r){db.ref('rooms/'+room+'/result').set(role+' '+r);} 
+function agree(){db.ref('rooms/'+room+'/ready/'+role).set(true);db.ref('rooms/'+room+'/ready').once('value').then(s=>{let d=s.val();if(d.p1&&d.p2){location.href='log.html?room='+room;}});}
